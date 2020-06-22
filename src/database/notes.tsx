adea21c9daad5e59ebb8140e0d1c92e9ec4e7bc7
Denis F <denisf.salles@gmail.com>
2020-06-22 14:11:35 -0300

    Finalizado os metodos crud da api com o banco de dados

--- src/database/notes.tsx
+++ src/database/notes.tsx
@@ -1,3 +1,14 @@
+// TODO : Criar um controller para controlar as regras de negócio de cada método do CRUD.
+interface NoteInterface {
+  note_id: number,
+  title: string,
+  content: string,
+  last_UpDate: string,
+  user_id: number
+}
+
+import { Alert } from 'react-native';
+
 let SQLite = require('react-native-sqlite-storage');
 let sqlite = SQLite.openDatabase({
   name: 'mydb.db',
@@ -5,23 +16,29 @@ let sqlite = SQLite.openDatabase({
   location: 'default',
 });
 
-const readNotes = () => {
+
+
+// Create
+const insertNewNote = (note: NoteInterface) => {
   return new Promise((resolve, reject) => {
+
+
+    if (!note || note === null) {
+      Alert.alert('Error', 'Nota vazia.');
+      return;
+    }
+
     sqlite.transaction((tx) => {
       tx.executeSql(
-        'SELECT * FROM note',
-        [],
+        `INSERT INTO note (title, content, date, user_id ) VALUES (?, ?, ?, ?);`,
+        [note.title, note.content, note.date, note.user_id],
         (tx, results) => {
-          let dataLength = results.rows.length;
-          let helperArrayDB = [];
-          if (dataLength > 0) {
-            for (let i = 0; i < dataLength; i++) {
-              helperArrayDB.push(results.rows.item(i));
-            }
-          }
-          resolve(helperArrayDB);
+          // Todo: Inserir algum log
+          Alert.alert('Inserido');
         },
         (error) => {
+          // Todo: Trabalhar no log de erro
+          console.log('Error', error);
           resolve(error);
         },
       );
@@ -29,19 +46,15 @@ const readNotes = () => {
   });
 };
 
-// Crate
-const insertNewNote = (note: any) => {
+// Read 
+const readNotes = () => {
   return new Promise((resolve, reject) => {
-    if (!note) {
-      return;
-    }
-
     sqlite.transaction((tx) => {
       tx.executeSql(
-        'INSERT INTO note (title, content, date, user_id ) VALUES (?) (?) (?) (?)',
-        ['note.title', 'note.content', 'note.date', 'user_id'],
-        (tx, results) => {
-          let dataLength = results.rows.length;
+        'SELECT * FROM note',
+        [],
+        (tx: any, results: any) => {
+          let dataLength: number = results.rows.length;
           let helperArrayDB = [];
           if (dataLength > 0) {
             for (let i = 0; i < dataLength; i++) {
@@ -50,7 +63,55 @@ const insertNewNote = (note: any) => {
           }
           resolve(helperArrayDB);
         },
+        (error: any) => {
+          resolve(error);
+        },
+      );
+    });
+  });
+};
+
+// Update
+const updateNote = (note: NoteInterface) => {
+  return new Promise((resolve, reject) => {
+
+
+    if (!note || note === null) {
+      Alert.alert('Error', 'Nota vazia.');
+      return;
+    }
+
+    sqlite.transaction((tx) => {
+      tx.executeSql(
+        `UPDATE note SET title = (?), content = (?), date = (?) WHERE note_id = (?)`,
+        [note.title, note.content, note.last_UpDate, note.note_id],
+        (tx, results) => {
+          // Todo: Inserir algum log
+          Alert.alert('Inserido');
+        },
         (error) => {
+          // Todo: Trabalhar no log de erro
+          console.log('Error', error);
+          resolve(error);
+        },
+      );
+    });
+  });
+};
+
+
+// Delete 
+const deleteNote = (note_id : number) => {
+  return new Promise((resolve, reject) => {
+    sqlite.transaction((tx) => {
+      tx.executeSql(
+        'DELETE FROM note WHERE note_id = (?)',
+        [note_id],
+        (tx: any, results: any) => {
+          Alert.alert('Deletado');
+        },
+        (error: any) => {
+          console.log('Error', error);
           resolve(error);
         },
       );
@@ -59,7 +120,9 @@ const insertNewNote = (note: any) => {
 };
 
 const notesDB = {
-  getNotesList: readNotes,
   insertNewNote: insertNewNote,
+  getNotesList: readNotes,
+  updateNote: updateNote,
+  deleteNote : deleteNote,
 };
 export default notesDB;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Button} from 'react-native';
 import {NotesList} from '../../components/NotesList';
 import notes from '../../database/notes';
 
 const Home = () => {
+  function onPressLearnMore() {
+    notes.deleteNote(1);
+  }
+
   return (
     <View>
       <NotesList />
+      <Button
+        onPress={onPressLearnMore}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 };
