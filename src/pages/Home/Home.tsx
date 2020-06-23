7d63aea229aa6e5de7373040f12fe78f744b27c1
Denis F <denisf.salles@gmail.com>
2020-06-23 03:23:41 -0300

    Adicionado react navegation, gesture handler e drawer navegation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { View, Button } from 'react-native';
 import { NotesList } from '../../components/NotesList';
 
-const Home = () => {
+const Home = ({ navigation }) => {
 
   return (
     <View>
+      <Button onPress={() => navigation.openDrawer()} title="Hamburger" />
       <NotesList />
     </View>
   );
